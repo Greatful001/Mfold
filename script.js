d5a9453fcd99475affe2236592e7397872eb0fb0
Greatful001 <111737576+Greatful001@users.noreply.github.com>
2023-08-05 23:17:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById("btn");
+const nav = document.getElementById("menu");
+
+
+const navToggle = (e) => {
+    btn.classList.toggle('active');
+    nav.classList.toggle('active');
+     
+//    document.body.classList.toggle('no-scroll')
+    if(nav.classList.contains('active')) {
+       btn.innerHTML = "CLOSE";
+    }
+    else {
+        btn.innerHTML = "MENU";
+    }
+} 
+
+
+btn.addEventListener('click', navToggle)
+
+// navlinks
+
+
+document.querySelectorAll(".navlinks").forEach(n => n.addEventListener
+    ("click", () => {
+        btn.classList.remove("active");
+        nav.classList.remove("active");
+        btn.innerHTML = "MENU";
+    }))
+
+
+    // AOS 
+    AOS.init({
+       
+        offset: 300,
+        duration: 1000
+    });
+
+
